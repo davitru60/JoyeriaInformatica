3a4e69d17cba7bf04d4c09118666dd547e2bae6a
Raul GM <loscanos3raul@gmail.com>
2023-11-22 19:03:33 +0100

    Update 22/11 Crear CRUD Componentes

---
+++ Backend/app/Models/Componentes.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Componentes extends Model
+{
+    use HasFactory; 
+    protected $table = 'componente';
+    protected $primaryKey = 'id_comp';
+    protected $fillable = [
+        'nombre'
+    ];
+}
--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('registrar',[AuthController::class,'registrar']);
 
+Route::get('/componente', [ComponentesController::class, 'index']); 
+Route::post('/componente', [ComponentesController::class, 'store']); 
+Route::get('/componente/{id}', [ComponentesController::class, 'buscar']); 
+Route::delete('/componente/{id}', [ComponentesController::class, 'destroy']); 
