0bf8529a93f0bcbfde2e3fcd6c1c1cf13f14a407
Raul GM <loscanos3raul@gmail.com>
2023-12-05 14:06:51 +0100

    CRUD joyas listo

--- Backend/app/Models/Joya.php
+++ Backend/app/Models/Joya.php
@@ -8,6 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Joya extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'id_joya';
+
     protected $table = 'joya';
 
     protected $fillable = [
